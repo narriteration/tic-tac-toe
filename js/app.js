6940c426bb4d5ffa89217005887c348b8a825f96
narriteration <narriteration@gmail.com>
2017-02-24 21:41:17 -0800

    Added basic game board structure using Bootstrap / Wrote first draft of pseudocode in JS file

--- js/app.js
+++ js/app.js
@@ -3,4 +3,22 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
+// set values of all id's (or all values w/in board?) to null
+// true or false: existNullPlayingSpaces?
+    // if F ===> function gameIsOver. If hasWinner, alert userSymbol "has won". Else, alert ("no winner this time, click reset to try again")
+    // if T ===>
+
+    // listen on the #board for an event: click USE jQuery
+    // true or false: click event has happened on #board
+        // F ===> loop again through algorithm
+        // T ===> on click :
+              // 1. GET the #id of event parent (?)
+              // 2. IF userSymbol is equal to X, SET contents of div #id with X; ELSE, SET contents of div #id with O
+              // 3. console.log("Great play. Who goes next?")
+
+
+
+
+
+
 });
